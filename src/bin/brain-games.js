cceb16ac2765b4646e0318ece56d4ab20c14c443
gorushkin <gorushkin@gmail.com>
2020-01-12 00:26:24 +0300

    step 5

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import hello from '..';
+import { hello } from '..';
 
 console.log('Welcome to the Brain Games!');
-console.log(hello());
+console.log(`Hello, ${hello()}!`);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const hello = () => {
   const userName = readlineSync.question('May I have your name? ');
-  return `Hello, ${userName}!`;
+  return userName;
+};
+
+export const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = hello();
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const maxTry = 3;
+  let i = 0;
+  while (i < maxTry) {
+    const number = Math.round(Math.random() * 100);
+    const question = (number % 2 === 0) ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== question) {
+      console.log(`yes' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${userName}!`);
+    } else {
+      i += 1;
+      console.log('Correct!');
+    }
+  }
 };
