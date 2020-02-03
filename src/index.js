cbb981c319265045a6b00e85ab86e0e4ca48baf9
gorushkin <gorushkin@gmail.com>
2020-02-03 21:16:09 +0300

    Самые-самые последние правки

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const tryCount = 3;
+const attemptsCount = 3;
 
 export default (generateQuestionAnswer, gameDescription) => {
   console.log('Welcome to the Brain Games!');
   console.log(gameDescription);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  for (let i = 0; i < tryCount; i += 1) {
+  for (let i = 0; i < attemptsCount; i += 1) {
     const [correctAnswer, question] = generateQuestionAnswer();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
       console.log(`${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
-      break;
-    } else {
-      console.log('Correct!');
-      if (i === 2) console.log(`Congratulations, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
+  console.log(`Congratulations, ${userName}!`);
 };
